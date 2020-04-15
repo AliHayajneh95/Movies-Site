f20abb5a6c05a7e45b74618ae62603cc85fd9197
AliHayajneh95 <Hayajnehali95@gmail.com>
2020-04-15 18:16:38 +0300

    this is html page with css and javascript and it contains while loop, and a for loop

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+'use strict'
+window.prompt("enter your age", "man you are so old, deal with it");
+var a = prompt('1+1 = ?');
+var a2 = prompt('What is your favorite movie?');
+var a3 = prompt('Type your 1st, 2nd, 3rd name');
+var res ;
+if (a == 2){
+    res = 'you are smart good';
+}
+else {
+    res = 'wrong try again';
+}
+document.write('<p id="a2">'+res+'</p>');
+document.write('<p id="a2">'+a2+'</p>');
+document.write('<p id="a3">'+a3+'</p>');
+
+function myFunction() {
+    var person = prompt("Please enter your name", "");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hello " + person + "! How are you today?";
+    }
+  }
+
+window.prompt("enter your age", "man you are so old, deal with it");
